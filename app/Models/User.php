fed2677a5c126d3a4c2655d4ed162302e2208538
Soffan Marsus Ahmad <soffandluffy@gmail.com>
2023-08-27 19:12:23 +0700

    add soft deletes to users

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -11,7 +12,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
     use HasRoles; //or HasFilamentShield
 
     /**
@@ -25,6 +26,7 @@ class User extends Authenticatable
         'email',
         'password',
         'active',
+        'deleted_at',
     ];
 
     /**
@@ -43,6 +45,7 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
+        'deleted_at' => 'datetime',
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('password');
             $table->boolean('active')->default(true);
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
